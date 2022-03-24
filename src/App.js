2e7e0f4cee10905869069d8247e24c2fe309a8be
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-24 22:17:48 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Upload to AWS v4</p>
+        <p>Upload to AWS v5</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 4
+          Learn React 5
         </a>
       </header>
     </div>

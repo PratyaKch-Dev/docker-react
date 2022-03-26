48b096a19981cd133990b8b3fbf91a050bbc8379
Zeeshan Ahmad <zeeshan.ahmad@sap.com>
2022-03-26 19:25:23 +0700

    change bucket

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Upload to AWS v7</p>
+        <p>Upload to AWS v7 change bucket</p>
         <a
           className="App-link"
           href="https://reactjs.org"
